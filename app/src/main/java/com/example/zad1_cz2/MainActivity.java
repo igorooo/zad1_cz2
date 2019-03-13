c00b79a1b6e1e8c131a921ca85d57631f2e4fd47
QiviPC <igorczeczot@windowslive.com>
2019-03-13 06:12:59 +0100

    13.03 05:12 (should be ok)

--- app/src/main/java/com/example/zad1_cz2/MainActivity.java
+++ app/src/main/java/com/example/zad1_cz2/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.zad1_cz2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button B1,B2,B3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        B1 = (Button) findViewById(R.id.button1);
+        B2 = (Button) findViewById(R.id.button2);
+        B3 = (Button) findViewById(R.id.button3);
+
+        B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+        B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+
+        B3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity4();
+            }
+        });
+
+
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void openActivity3(){
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+
+    public void openActivity4(){
+        Intent intent = new Intent(this, Main4Activity.class);
+        startActivity(intent);
     }
 }
